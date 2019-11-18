2529999c27e49e4ae67a327d6faec96d74ee38a7
Zexu Jin <54969533+ZexuJin@users.noreply.github.com>
2019-11-18 12:54:22 -0800

    Update AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styles from './AboutUs.module.css'
+
+const AboutUs = () => {
+    return (
+        <div>
+
+            <h1>
+                About Us
+            </h1>
+            <section>
+                <h2>
+                    Developer Name: <br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    <img src="/images/treefrog.jpg" /><br/>
+                </h2>
+                <h2>
+                    Developer Name:<br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    <img src="/images/treefrog.jpg" /><br/>
+                </h2>
+                <h2>
+                    Developer Name:<br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    <img src="/images/treefrog.jpg" /><br/>
+                </h2>
+                <h2>
+                    Developer Name:<br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    Bla:<br/>
+                    <img src="/images/treefrog.jpg" /><br/>
+                </h2>
+            </section>
+
+        </div>
+    )
+}
+
+export default AboutUs
