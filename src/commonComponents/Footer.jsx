89cee8267f24357a3221573999b45c44b649e930
christine zhang <zhang.xuanw@husky.neu.edu>
2019-11-18 16:02:31 -0800

    add basic footer

---
+++ src/commonComponents/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+
+const Footer = (props) => {
+    return (
+        <div className={styles.footer}>
+            <div><a href="/aboutus">About Us</a></div>
+        </div>
+    )
+}
+
+export default Footer
