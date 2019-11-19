164a547b3818b910f55a228c09558bdf221a9ac2
christine zhang <zhang.xuanw@husky.neu.edu>
2019-11-18 17:44:36 -0800

    unify id and add resolve id in detail page

--- src/DishDetailPage.jsx
+++ src/DishDetailPage.jsx
@@ -3,9 +3,12 @@ import dishes from "./data/dishes.json";
 import DishDetail from "./DishDetail.jsx"
 
 const DishDetailPage = (props) => {
-    
+    let dishMap=new Map();
+    for(let i in dishes){
+        dishMap.set(dishes[i].id,dishes[i]);
+    };
     return (
-        <DishDetail dish={Object.values(dishes)[0]}/>
+        <DishDetail dish={dishMap.get(parseInt(props.id))}/>
     )
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,14 +7,14 @@ const Home = () => {
         
         <section>
             <div className={styles.big_grid_1}><img src="/images/treefrog.jpg"></img></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
-            <div><a href="/detail/1"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1001"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1002"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1003"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1004"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1005"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/1006"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/3001"><img src="/images/treefrog.jpg" href></img></a></div>
+            <div><a href="/detail/3002"><img src="/images/treefrog.jpg" href></img></a></div>
             <div className={styles.big_grid_2}><img src="/images/treefrog.jpg"></img></div> 
         </section>
     )
