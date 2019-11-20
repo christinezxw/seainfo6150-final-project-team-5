0216b8fb6b1741cdfbc5fd891d6685da5bc0fd6b
Zexu Jin <54969533+ZexuJin@users.noreply.github.com>
2019-11-20 15:01:36 -0800

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -3,41 +3,37 @@ import styles from './AboutUs.module.css'
 
 const AboutUs = () => {
     return (
-        <div>
-
-            <h1>
-                About Us
+        <div className={styles.div}>
+            <h1 className={styles.header}>
+            About the website<br/>
             </h1>
-            <section>
-                <h2>
-                    Developer Name: <br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    <img src="/images/treefrog.jpg" /><br/>
-                </h2>
-                <h2>
-                    Developer Name:<br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    <img src="/images/treefrog.jpg" /><br/>
-                </h2>
-                <h2>
-                    Developer Name:<br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    <img src="/images/treefrog.jpg" /><br/>
-                </h2>
-                <h2>
-                    Developer Name:<br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    Bla:<br/>
-                    <img src="/images/treefrog.jpg" /><br/>
-                </h2>
-            </section>
+            <p className={styles.text}>
+
+                Our mission is to provide people<br/>
+                a welcomed, colorful website of dishes <br/>
+                and make it useful and fully accessible.<br/><br/><br/>
+            </p>
+
+            <h1 className={styles.header}>
+                Who are we?<br/>
+            </h1>
+            <p className={styles.text}>
+
+                We are a group of four.<br/>
+                This is the final project of the SEAINFO6150 class.<br/>
+                Enjoy your journey here!<br/><br/><br/>
+            </p>
+
+            <h1 className={styles.header}>
+                How to contact?<br/>
+            </h1>
+            <p className={styles.text}>
+
+                If your have any question.<br/>
+                Please feel free to contact us.<br/>
+                email: XXXXXXX@husky.neu.edu <br/><br/><br/>
+            </p>
+
 
         </div>
     )
