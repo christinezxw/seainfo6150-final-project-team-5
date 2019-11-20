54ef8762bf4a7d0a7a322829c731aac36e79d777
bates-hu <hbc0204@foxmail.com>
2019-11-20 01:21:52 -0800

    remove the mock img and link of home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styles from './Home.module.css'
 import Card from './commonComponents/Card'
+import dishes from "./data/dishes.json"
 
 
 
@@ -33,27 +34,27 @@ export default class Home extends Component {
             <div>
                 <p>screen width : {this.state.width}</p>
                 <p>code point 1</p>
-                <p><a href="#">Thanks Giving Category</a></p>
-                <p><a href="#">Dessert Menu Category</a></p>
-                <p><a href="#">Chinese Food Category</a></p>
-                <p><a href="#">Soup Category</a></p>
+                <p><a href="/thanksgiving recipe">thanksgiving recipe</a></p>
+                <p><a href="/Dessert">Dessert Menu Category</a></p>
+                <p><a href="/Chrismas Recipe">Chrismas Recipe</a></p>
+                <p><a href="/soup">Soup Category</a></p>
             </div>
             );
         } else if (this.state.width < 800) {
+
             return (
                 <div>
                     <p>screen width : {this.state.width}</p>
                     <p>code point 2</p>
-                    <Card imgUrl="/images/treefrog.jpg" desc="1001" link="/detail/1001"/>
-                    <Card imgUrl="/images/treefrog.jpg" desc="1002" link="/detail/1002"/>
-                    <Card imgUrl="/images/treefrog.jpg" desc="1003" link="/detail/1003"/>
-                    <Card imgUrl="/images/treefrog.jpg" desc="1004" link="/detail/1004"/>
-                    <Card imgUrl="/images/treefrog.jpg" desc="1005" link="/detail/1005"/>
-                    <Card imgUrl="/images/treefrog.jpg" desc="1006" link="/detail/1006"/>
-                    <Card imgUrl="/images/treefrog.jpg" desc="3001" link="/detail/3001"/>
+                    {
+                        dishes.map(dish => (
+                            <Card imgUrl={dish.img.url} desc={dish.name} link={"/detail/" + dish.id}/>
+                        ))
+                    }
                 </div>
                 );
         } else {
+            var showDishes = new Set([1001, 1002, 1003, 1004, 1005, 1006, 3001, 3002]);
             return (
                 <div>
                 <p>screen width : {this.state.width}</p>
@@ -62,19 +63,32 @@ export default class Home extends Component {
                 
                 <div className={styles.big_grid_1}>
                     <h1>Soup</h1>
-                    <a href ="/soup"><img src="/images/treefrog.jpg" href></img></a>
+                    <a href ="/soup"><img src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2010/11/1/2/FNM_120110-Ask-Ellie-001_s4x3.jpg.rend.hgtvcom.826.620.suffix/1382539602880.jpeg" href></img></a>
                 </div>
-                <div><a href="/detail/1001"><img src="/images/treefrog.jpg" href></img></a></div>
-                <div><a href="/detail/1002"><img src="/images/treefrog.jpg" href></img></a></div>
-                <div><a href="/detail/1003"><img src="/images/treefrog.jpg" href></img></a></div>
-                <div><a href="/detail/1004"><img src="/images/treefrog.jpg" href></img></a></div>
-                <div><a href="/detail/1005"><img src="/images/treefrog.jpg" href></img></a></div>
+                {
+                    dishes
+                    .filter(dish => dish.category === "thanksgiving recipe")
+                    .slice(0,4)
+                    .map(dish => (<div><a href={"/detail/" + dish.id}><img src={dish.img.url}></img></a></div>))
+                }
+                {
+                    dishes
+                    .filter(dish => dish.category === "soup")
+                    .slice(0,4)
+                    .map(dish => (<div><a href={"/detail/" + dish.id}><img src={dish.img.url}></img></a></div>))
+                }
+                
+                {/* <div><a href="/detail/1001"><img src="/images/treefrog.jpg"></img></a></div>
+                <div><a href="/detail/1002"><img src="/images/treefrog.jpg"></img></a></div>
+                <div><a href="/detail/1003"><img src="/images/treefrog.jpg"></img></a></div>
+                <div><a href="/detail/1004"><img src="/images/treefrog.jpg"></img></a></div>
+                <div><a href="/detail/1005"><img src="/images/treefrog.jpg"></img></a></div>
                 <div><a href="/detail/1006"><img src="/images/treefrog.jpg" href></img></a></div>
                 <div><a href="/detail/3001"><img src="/images/treefrog.jpg" href></img></a></div>
-                <div><a href="/detail/3002"><img src="/images/treefrog.jpg" href></img></a></div>
+                <div><a href="/detail/3002"><img src="/images/treefrog.jpg" href></img></a></div> */}
                 <div className={styles.big_grid_2}>
                     <h1>Thanksgiving</h1>
-                    <a href ="/thanksgiving recipe"><img src="/images/treefrog.jpg" href></img></a>
+                    <a href ="/thanksgiving recipe"><img src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/3/26/0/BX0808H_balsamic-roasted-brussels-sprouts-recipe_s4x3.jpg.rend.hgtvcom.826.620.suffix/1402583469880.jpeg" href></img></a>
                 </div> 
                 </div>
                 </div>
