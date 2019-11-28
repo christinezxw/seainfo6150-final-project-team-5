b4d959c1fcb873a2dfdd229651aa0844f3f0f304
Zexu Jin <54969533+ZexuJin@users.noreply.github.com>
2019-11-27 20:38:44 -0800

    Update SubscribeForm.jsx

--- src/SubscribeForm.jsx
+++ src/SubscribeForm.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import {Col, Button,Form, FormGroup,Label,Input} from "reactstrap";
 import styles from "./SubscribeForm.module.css"
 
 export default class SubscribeForm extends Component {
