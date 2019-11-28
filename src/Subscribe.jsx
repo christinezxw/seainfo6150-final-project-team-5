1d71ea10b2cc75eed402858a7d0f70aa80b317b9
Zexu Jin <54969533+ZexuJin@users.noreply.github.com>
2019-11-27 20:32:18 -0800

    Update Subscribe.jsx

--- src/Subscribe.jsx
+++ src/Subscribe.jsx
@@ -1,81 +1,20 @@
-import React, { Component } from "react";
-import styles from "./Subscribe.module.css";
+import React from "react";
+import SubscribeForm from './SubscribeForm.jsx';
+import styles from './Subscribe.module.css';
 
-export default class Subscribe extends Component {
-    handleChange;
-    constructor(props) {
-        super(props);
-        this.state = {
-            submittedForm: null
-        };
-    }
+const Subscribe =(props) => {
+    return (
+        <div>
+            <header className = {styles.header}>
+                SUBSCRIBE FOR MORE!!
+            </header><br/>
+            <form>
+                <SubscribeForm/>
+            </form><br/>
+        </div>
 
-    handleSubmit(event) {
-        alert('Thank you for your subscribe!');
-        event.preventDefault();
-    }
 
-    render() {
-        return(
-            <div className={styles.div}>
-
-                <header className = {styles.header}>
-                    Subscribe Page<br/><br/>
-                </header>
-
-                <section className = {styles.form}>
-                    <form onSubmit={this.handleSubmit}>
-
-                        <label>
-                            First Name:
-                            <input type="firstName" placeholder="First Name" name="text1" /><br/>
-                        </label>
-
-                        <label>
-                            Last Name:
-                            <input type="lastName" placeholder="Last Name" name="text2" /><br/>
-                        </label>
-
-                        <label>
-                            Your Sex:
-                            <select value={this.state.value} onChange={this.handleChange}>
-                                <option value="male">Male</option>
-                                <option value="female">Female</option>
-                                <option value="null">Prefer not to tell</option>
-                            </select><br/>
-                        </label>
-
-                        <label>
-                            Email Address:
-                            <input type="emailAddress" placeholder="Email Address" name="text3" /><br/>
-                        </label>
-
-                        <label>
-                            Your Favorite Category:
-                            <select value={this.state.value} onChange={this.handleChange}>
-                                <option value="thanksgiving">Thanksgiving</option>
-                                <option value="christmas">Christmas</option>
-                                <option value="soups">Soups</option>
-                                <option value="desserts">Desserts</option>
-                            </select><br/>
-                        </label>
-                        
-                        <label>
-                            Frequency
-                            <select value={this.state.value} onChange={this.handleChange}>
-                                <option value="Per Day">Per Day</option>
-                                <option value="Per Week">Per Week</option>
-                                <option value="Per Month">Per Month</option>
-                            </select><br/>
-                        </label>
-                        
-                        <label>
-                            <input type="submit" value="submit" />
-                        </label>
-                    </form>
-                </section>
-
-            </div>
-        );
-    }
+    )
 }
+
+export default Subscribe;
